055ab232b40384078b61f559e340f4defb0b827a
ation_ciger <ation_ciger@sgxi.cn>
2026-02-21 21:33:44 +0800

    ``` refactor(BoardConfig): 移除内核模块相关配置
    移除了已废弃的内核模块配置项，包括 BOARD_VENDOR_RAMDISK_KERNEL_MODULES
    和 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD 变量定义，
    以简化 BoardConfig.mk 文件结构。
    ```

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,10 +34,6 @@ BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD) $(BO
 # TODO: 替换为 rubens 的 defconfig 文件名
 TARGET_KERNEL_CONFIG := mikrn_rubens_stability_defconfig
 
-# Kernel modules
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)/prebuilts/modules/*.ko)
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/prebuilts/modules/modules.load))
-
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
